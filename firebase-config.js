8524116616ecb2f97495f006a6f6cef5165a3f1f
Will <williamaallred-cmyk@users.noreply.github.com>
2025-11-22 09:40:27 -0500

    Add Firebase config for deployment

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAbNRF1XT3VYW8R-Mm21Ao4NItlyv9_1U",
+  authDomain: "budgettracker-a675c.firebaseapp.com",
+  projectId: "budgettracker-a675c",
+  storageBucket: "budgettracker-a675c.firebasestorage.app",
+  messagingSenderId: "892922279146",
+  appId: "1:892922279146:web:f9bab3ce50cb5c66431393"
+};
